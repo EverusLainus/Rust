399a38f02e807390d0284bcbda59cdedbf09dfdd
Everus <everuslainus@gmail.com>
2024-03-19 20:53:01 +0530

    fn

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,14 @@
+fn operations(a: i32, b: i32) -> i32{
+    return a * b;
+}
+
 fn main() {
     let mut x = 5;
     //error cas i has space like in { x}
     println!("value of x is {x}");
     {
-    let mut x = x+x; //showdowing
+    let x = x+x; //showdowing
     println!("value of x is {x}");
     }//shadowing scope ends here.
-    println!("value of x becomes  {x}");
+    println!("value of x becomes  { }", operations(2,3));
 }
